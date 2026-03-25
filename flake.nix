4094b119abd56632a31b9b1dc3b4f3412f19ac87
Adam Cvikl <68786400+Adamekka@users.noreply.github.com>
2026-03-25 15:09:35 +0100

    dev: remove C++ modules support

--- flake.nix
+++ flake.nix
@@ -26,58 +26,15 @@
                             "-L${llvm.libcxx}/lib"
                             "-Wl,-rpath,${llvm.libcxx}/lib"
                         ];
-                        libcxxModules = pkgs.writeText "libc++.modules.json" (builtins.toJSON {
-                            version = 1;
-                            revision = 1;
-                            modules = [
-                                {
-                                    logical-name = "std";
-                                    source-path = "${llvm.libcxx}/share/libc++/v1/std.cppm";
-                                    is-std-library = true;
-                                    local-arguments = {
-                                        system-include-directories = [
-                                            "${llvm.libcxx.dev}/include/c++/v1"
-                                        ];
-                                    };
-                                }
-                                {
-                                    logical-name = "std.compat";
-                                    source-path = "${llvm.libcxx}/share/libc++/v1/std.compat.cppm";
-                                    is-std-library = true;
-                                    local-arguments = {
-                                        system-include-directories = [
-                                            "${llvm.libcxx.dev}/include/c++/v1"
-                                        ];
-                                    };
-                                }
-                            ];
-                        });
-                        clangxxWrapper = pkgs.writeShellScriptBin "clang++" ''
-                            set -euo pipefail
-
-                            compiler='${llvm.libcxxClang}/bin/clang++'
-                            libcxx_modules='${libcxxModules}'
-
-                            for arg in "$@"; do
-                                case "$arg" in
-                                    -print-file-name=libc++.modules.json)
-                                        printf '%s\n' "$libcxx_modules"
-                                        exit 0
-                                        ;;
-                                esac
-                            done
-
-                            exec "$compiler" "$@"
-                        '';
                     in
                     f {
                         inherit commonCxxFlags commonLdFlags;
                         inherit pkgs system;
-                        inherit clangxxWrapper llvm;
+                        inherit llvm;
                     }
                 );
         in {
-            devShells = forAllSystems ({ pkgs, clangxxWrapper, commonCxxFlags, commonLdFlags, llvm, ... }: {
+            devShells = forAllSystems ({ pkgs, commonCxxFlags, commonLdFlags, llvm, ... }: {
                 default = pkgs.mkShell {
                     hardeningDisable = [
                         "fortify"
@@ -85,24 +42,19 @@
                     ];
 
                     packages = [
-                        clangxxWrapper
+                        llvm.libcxxClang
                         pkgs.cmake
                         pkgs.ninja
                         llvm.libcxx
-                        llvm.libcxxClang
                         llvm.clang-tools
                     ];
 
                     CXXFLAGS = commonCxxFlags;
                     LDFLAGS = commonLdFlags;
-
-                    shellHook = ''
-                        export PATH='${clangxxWrapper}/bin:'"$PATH"
-                    '';
                 };
             });
 
-            packages = forAllSystems ({ pkgs, clangxxWrapper, commonCxxFlags, commonLdFlags, llvm, ... }: {
+            packages = forAllSystems ({ pkgs, commonCxxFlags, commonLdFlags, llvm, ... }: {
                 default = llvm.libcxxStdenv.mkDerivation {
                     pname = "visual-novel-engine";
                     version = "0.1.0";
@@ -130,11 +82,10 @@
                     ];
 
                     nativeBuildInputs = [
-                        clangxxWrapper
+                        llvm.libcxxClang
                         pkgs.cmake
                         pkgs.ninja
                         llvm.libcxx
-                        llvm.libcxxClang
                     ];
 
                     CXXFLAGS = commonCxxFlags;
@@ -142,7 +93,7 @@
 
                     cmakeGenerator = "Ninja";
                     cmakeFlags = [
-                        "-DCMAKE_CXX_COMPILER=${clangxxWrapper}/bin/clang++"
+                        "-DCMAKE_CXX_COMPILER=${llvm.libcxxClang}/bin/clang++"
                         "-DCMAKE_CXX_STANDARD_LIBRARY=libc++"
                     ];
                 };
